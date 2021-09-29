1470fbd890f1f3494f82162fb025ed131a6b4ba7
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-29 09:49:45 +0530

    Create Enum.java

---
+++ Enum.java
@@ -0,0 +1,16 @@
+enum Languange 
+  {
+    C,
+    JAVA,
+    PHP,
+    ANDROID
+  } 
+class Program 
+{
+  public static void main(String[] args) 
+  {
+   /*creating variable of enum*/
+    Languange myVar = Languange.ANDROID; 
+    System.out.println(myVar);
+  }
+}
