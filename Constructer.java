abdd60051a324deafb0881a16e8654e76e985a60
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2021-09-29 08:12:54 +0530

    Create Constructer.java

---
+++ Constructer.java
@@ -0,0 +1,17 @@
+class square
+{
+int side;
+int area;
+//constructor 
+Rectangle()
+ {
+  side=30
+  area=side*side
+  System.out.println("Area of square="+area);
+ }
+ public static void main(String[] args) 
+ {
+  //creating object
+  Rectangle obj=new Rectangle();
+ }
+}
